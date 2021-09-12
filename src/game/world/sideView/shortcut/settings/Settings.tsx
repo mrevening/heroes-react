86252ddb79788b275fda79f66d2217734bcb7895
dwieczorek <dwieczorek@kpmg.pl>
2021-09-12 13:59:58 +0200

    back to main menu

--- src/game/world/sideView/shortcut/settings/Settings.tsx
+++ src/game/world/sideView/shortcut/settings/Settings.tsx
@@ -1,7 +1,15 @@
+import { Button } from 'reactstrap'
+import { useAppDispatch } from 'state/hooks'
+import { setShowMainManu } from 'app/AppSlice'
+
 export default function Settings(){
+    const dispatch = useAppDispatch();
+
     return(
         <>
-            <div>Settings</div>
+            <div>
+                <Button color="primary" onClick={() => { dispatch(setShowMainManu())} }>Go Back to main menu</Button>
+            </div>
         </>
     )
 }
